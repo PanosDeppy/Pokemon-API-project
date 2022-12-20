6af0cc8682263535946b55e7c493ca2fb9f2e272
PanosDeppy <panagiotiswb@gmail.com>
2022-12-20 17:46:25 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,34 @@
+const searchButton = document.querySelector('#myButton');
+
+function search(e) {
+  e.preventDefault();
+  let pokemonName = document.querySelector('#userInput').value;
+  let resultsMoves = document.querySelector('#results');
+
+  // API based on the pokemon searched
+  let api = `https://pokeapi.co/api/v2/pokemon/${pokemonName}`;
+
+  // request the data
+  fetch(api)
+    .then((response) => {
+        return response.json();
+    })
+    .then((data) => {
+        let pokeMoves = data["moves"];
+        //pokeMoves[0]["move"]["name"] => to get the first move
+        let pokeValues = Object.values(pokeMoves);
+
+        let allMoves = [];
+        for (const move in pokeMoves) {
+            allMoves.push(`<p>${pokeMoves[move]["move"]["name"]}</p>`);
+        }
+        
+        resultsMoves.innerHTML = `<p> Check below the moves: ${allMoves.join(" ")}. </p>`;
+        console.log(typeof Object.values(pokeValues));
+    })
+    .catch((error) => {
+        console.log(error);
+    });
+}
+
+searchButton.addEventListener('click', search);
