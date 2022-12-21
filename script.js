596a8b536eb0eb3a2bbe2768beffb0cbf6a40b15
PanosDeppy <panagiotiswb@gmail.com>
2022-12-21 19:13:51 +0000

    style and js changes

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
 const searchButton = document.querySelector('#myButton');
-searchButton.addEventListener('click', search);
+
 
 function search(e) {
   e.preventDefault();
   let pokemonName = document.querySelector('#userInput').value;
   let resultsMoves = document.querySelector('#results');
-  let pokeImage = document.querySelector('#pokemonImage');
+  let pokemonImage = document.querySelector('#pokemon-image');
 
   // API based on the pokemon searched
   let api = `https://pokeapi.co/api/v2/pokemon/${pokemonName}`;
-  
 
   // request the data
   fetch(api)
@@ -19,17 +18,27 @@ function search(e) {
     .then((data) => {
         let pokeMoves = data["moves"];
         //pokeMoves[0]["move"]["name"] => to get the first move
+        let pokeValues = Object.values(pokeMoves);
 
         let allMoves = [];
-
         for (const move in pokeMoves) {
-            allMoves.push(`${pokeMoves[move]["move"]["name"]}`);
+            allMoves.push(`<p>${pokeMoves[move]["move"]["name"]}</p>`);
         }
-        pokeImage.innerHTML = `<img src= "https://img.pokemondb.net/artwork/large/${pokemonName}.jpg">`;
-        resultsMoves.innerHTML = `Check below the moves: ${allMoves.join(", ")}. `;
+        
+       // Get the URL of the default front sprite of the Pokemon
+       const spriteUrl = data.sprites.front_default;
+
+       //A Sprite is a small graphic image file from the website
+       
+       //Assign the URL of the sprite as the value of the src attribute of the pokemonImage element.
+       pokemonImage.src = spriteUrl;
+
+        resultsMoves.innerHTML = `<h2>Moves:</h2><ul id="moves-list"><li>${allMoves.join("</li><li>")}</li></ul>`;
         console.log(typeof Object.values(pokeValues));
     })
     .catch((error) => {
         console.log(error);
     });
 }
+
+searchButton.addEventListener('click', search);
